a4c0a07d3a89d958a5bb7976124e83e2cfba23b3
Neha97ganga <ganagllanehasri97@gmail.com>
2026-02-12 14:51:48 +0530

    Prepared backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -209,6 +209,7 @@ app.post("/generate-supplier-docs", async (req, res) => {
 });
 
 /* ---------- Start Server ---------- */
-app.listen(5000, () => {
-  console.log("🚀 Backend running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("🚀 Backend running on port", PORT);
 });
